5952c68a25c671079f4935d4fdfa9d974f0f664e
gustavokf2003 <gustavokf2003@gmail.com>
2022-09-30 11:04:09 -0300

    botZangado modificado

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -6,13 +6,11 @@ class BotZangado(Bot):
         "Quero um conselho": "Meu conselho é que você vá embora. Eu não quero conversar com você!",
         "Quero uma piada": "Não sou palhaço para contar uma piada para você. Vá embora!"})
 
-
     def apresentacao(self):
         return f"{self.nome} diz: Estou dormindo escolha outro bot."
  
-    def mostra_comandos(self):
-        pass
-    
+    def executa_comando(self):
+        pass   
 
     def boas_vindas(self):
         return f"{self.nome} diz: Porque você me escolheu. Eu te odeio!" 
