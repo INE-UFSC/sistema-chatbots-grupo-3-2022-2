2da34a1099088ad3a017ac16141bca3594029df8
Lucas <lucascsoares21@gmail.com>
2022-09-30 10:26:35 -0300

    Arrumado o erro nos comandos

--- Bots/BotTriste.py
+++ Bots/BotTriste.py
@@ -3,8 +3,8 @@ from Bots.bot import Bot
 
 class BotTriste(Bot):
     def __init__(self, nome):
-        comandos = {"Qual o seu nome?": "Meu nome é tristeza. Na verdade é %s mas é quase a mesma coisa..." % nome"
-        "Quero um conselho:": "Meu conselho é você não me perguntar nada. Eu não sei nada."
+        comandos = {"Qual o seu nome?": "Meu nome é tristeza. Na verdade é %s mas é quase a mesma coisa..." % nome,
+        "Quero um conselho:": "Meu conselho é você não me perguntar nada. Eu não sei nada.",
         "Quero uma piada:": "Eu não sei contar piadas. Eu sou triste."}
 
         super().__init__(nome, comandos)
